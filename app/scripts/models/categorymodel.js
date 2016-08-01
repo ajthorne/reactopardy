abdb949a45f13cb43c989cbd50639a5e41f63514
Amanda Thorne <manda.thorne@gmail.com>
2016-07-31 20:39:39 -0500

    testing

--- app/scripts/models/categorymodel.js
+++ app/scripts/models/categorymodel.js
@@ -1,7 +1,7 @@
 import Backbone from 'backbone';
 
 const CategoryModel = Backbone.Model.extend({
-    urlRoot: 'http://jservice.io/api/categories?count=6',
+    urlRoot: 'https://jservice.io/api/categories?count=6',
 
     defaults: {
       id: '',
